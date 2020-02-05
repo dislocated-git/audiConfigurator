38e1e64e77931f5fef4faf4c07590bc3243423bf
17u-asamoilov <17u-asamoilov@UTCCD182>
2020-02-05 10:50:01 +0000

    Add project files.

---
+++ AudiConfigurator/Car.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudiConfigurator
+{
+    class Car
+    {
+        public List<IComponent> components { get; set; }
+
+        public Trim Trim { get; set; }
+
+    }
+}
---
+++ AudiConfigurator/IComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudiConfigurator
+{
+    interface IComponent
+    {
+        public String Name { get; };
+        public int ID { get; };
+        public int Price { get; }
+    }
+}
